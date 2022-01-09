c00a86270fd8210a9854ab215030742d2db2cf46
전은형 <ooooo5h91@gmail.com>
2022-01-09 21:50:47 +0900

    실제 FCM서버에 보내줄 내용 작성

--- main.py
+++ main.py
@@ -16,4 +16,14 @@ def send_fcm_notification(title, body):
         'Authorization' : 'key=AAAA5KK-mvQ:APA91bGfDkYHgt4ihRKsgQGY9IYkl4a7rawsS0_Hmyz77KiO-SIz8ymd4j1KjEht3uCWbS0VnMChtwqk5LKOCadzyQlQdX6GOfuZEBUufTAd6xdB3V6BziX080vh5kNmAjZqJwbfn1gM',
         'Content-Type' : 'application/json; UTF-8',
     }
+    
+    content = {
+        'registration_ids' : 'dmsIsoVpSWy__3REXQXVmP:APA91bFeLGvPm0AkY1YyF25pwDW3PQXcRK0aZO-0oVtaXklIbajFNERVFePjytnt2aS5DDK_cDHFT3IXZhy5hs27PonueoBq0IK5pU7DXEHnM0b8GQfeEA_I_dE72t4shNLj4qAIaQFF', # 어느 기기에 보낼건지, 디바이스 토큰  cf) 리스트로 넣으면 여러기기에 동시전송
+        'notification' : {
+            
+            'title' : title,
+            'body' : body,
+            }, # 기본양식의 알림. data-message로 보내면 커스터마이징할수있는 지원을 해주는 알림
+    }
+    
     # 3. 어떤 방식
